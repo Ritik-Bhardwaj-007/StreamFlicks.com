47ef7efe8708c0da89772f653f2740af53862a31
Ritik Bhardwaj <ritiksharma0748@gmail.com>
2024-02-29 16:14:43 +0530

    added aggregation pipeline

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -268,6 +268,73 @@ const updateUserCoverImage= asyncHandler(async (req,res)=>{
    new ApiResponse(200,user,"Cover Image Updated Successfully ")
    )
 })
+ const getUserChannelProfile= asyncHandler(async(req,res)=>{
+       const {username} = req.params;
+       if(!username){
+        throw new ApiError(400,'Username Is Missing')
+       }
+       const channel= await User.aggregate([
+        {
+            $match:{
+                username:username?.toLowerCase()
+            }
+        },
+        {
+            $lookup:{
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:"channel",
+                as:"subscribers"                
+            }
+        },
+        {
+            $lookup:{
+                from :"subscriptions",
+                localField:"_id",
+                foreignField:"subscriber",
+                as:"subscribedTo"
+            }
+        },
+        {
+            $addFields:{
+               subscribersCount:{
+                $size:"$subscribers"
+               },
+               channelsSubscribedTOCount:{
+                $size:"$subscribedTo"
+               },
+               isSubscribed:{
+                 $cond:{
+                    if:{
+                        $in:[req.user?._id,"$subscribers.subscriber"]  
+                    },
+                    then:true,
+                    else:false
+                 }
+               }
+            }
+        },
+        {
+         $project:{
+            fullname:1,
+            username:1,
+            subscribersCount:1,
+            channelsSubscribedTOCount:1,
+            isSubscribed:1,
+            avatar:1,
+            coverImage:1,
+            email:1,
+         }
+        }
+       ])
+       if(!channel?.lenght){
+        throw new ApiError(404,"channel doesn't exists")
+       }
+
+       return  res.status(200).json(
+        new ApiResponse(200,channel[0],"user channel fetched Successfully ")
+       )
+ })
 export {
     registerUser,
     loginUser,
@@ -277,5 +344,6 @@ export {
     getCurrentUser,
     updateAccountDetails,
     updateUserAvatar,
-    updateUserCoverImage
+    updateUserCoverImage,
+    getUserChannelProfile,
 };
